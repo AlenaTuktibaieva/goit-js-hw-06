e05f000df0aebe4ba661a0512355c25758156bd7
Alena Tuktibaieva <alenatuktibaieva@gmail.com>
2022-02-04 19:13:26 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -11,9 +11,11 @@ const list = document.querySelector('ul');
 const element = document.createElement("li");
 console.log(element);
 
-const item = ingredients.map((ingredients) => `<li class="list-item">${ingredients}</li>`)
+const item = ingredients.map((ingredient) => `<li>${ingredient}</li>`)
   .join("");
+console.log(item);
   
-  console.log(item);
+element.classList.add("item");
+console.log(element.classList);
 
 list.innerHTML += item;
