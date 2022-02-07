6feb687a21a1ba95d45e9f6bdef00602068002ec
Alena Tuktibaieva <alenatuktibaieva@gmail.com>
2022-02-07 22:41:55 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+
+})
